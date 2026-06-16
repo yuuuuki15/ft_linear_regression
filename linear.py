c166101b1204a32405d462dec963a8bd84dc61a2
yuuuuki15 <rx86340616@gmail.com>
2026-06-16 17:24:18 +0200

    draw line

--- linear.py
+++ linear.py
@@ -2,18 +2,29 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def load_model():
+    try:
+        model = pd.read_csv('model/default.csv')
+        return model['theta0'][0], model['theta1'][0]
+    except FileNotFoundError:
+        return 0, 0
+
 data = pd.read_csv('data.csv')
+theta0, theta1 = load_model()
 
 print(data.head())
 
 x = data['km']
 y = data['price']
+line_x = [min(data['km']), max(data['km'])]
+line_y = [theta0 + theta1 * min(data['km']), theta0 + theta1 * max(data['km'])]
 
 image_folder_name = 'images'
 timestamp = pd.Timestamp.now().strftime('%Y%m%d_%H%M%S')
 file_name = f'{image_folder_name}/price_vs_km_{timestamp}.png'
 
 plt.scatter(x, y)
+plt.plot(line_x, line_y, color='red')
 plt.xlabel('Kilometers')
 plt.ylabel('Price')
 plt.title('Price vs Kilometers')
